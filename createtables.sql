aa55e455f71ce4297d7b9678c168d1a110fa8ac0
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-04-17 17:07:56 -0400

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -27,4 +27,17 @@ CREATE TABLE `twitterhive` (
   `location` STRING,
   `user_mentions` STRING,
   `user_description` STRING
-)
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `twitter` (`msg` STRING, `unixtime` STRING, `friends_count` STRING, `hashtags` STRING, 
+                                               `listed_count` STRING, `tweet_id` STRING, `user_name` STRING, 
+                                               `favourites_count` STRING, `source` STRING, `placename` STRING, 
+                                               `media_url` STRING, `retweet_count` STRING, 
+                                               `user_mentions_name` STRING, `geo` STRING,
+                                               `urls` STRING, `countrycode` STRING, 
+                                               `user_url` STRING, `place` STRING, `coordinates` STRING,
+                                               `handle` STRING, `profile_image_url` STRING, `time_zone` STRING,
+                                               `ext_media` STRING, `statuses_count` STRING, `followers_count` STRING, 
+                                               `location` STRING, `user_mentions` STRING, `user_description` STRING) 
+STORED AS ORC
+LOCATION '/tmp/cnow3'
