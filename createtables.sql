c878ce936c4b83d9db2ebd1522fc4afd01a06ef0
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-04-22 20:55:06 -0400

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -40,4 +40,18 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `twitter` (`msg` STRING, `unixtime` STRING, 
                                                `ext_media` STRING, `statuses_count` STRING, `followers_count` STRING, 
                                                `location` STRING, `user_mentions` STRING, `user_description` STRING) 
 STORED AS ORC
-LOCATION '/tmp/cnow3'
+LOCATION '/tmp/cnow3';
+
+
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `sensors` (`uuid` STRING, `amplitude100` DOUBLE, 
+`amplitude500` DOUBLE, `amplitude1000` DOUBLE, `lownoise` DOUBLE, `midnoise` DOUBLE,
+`highnoise` DOUBLE, `amps` DOUBLE, `ipaddress` STRING, `host` STRING, `host_name` STRING,
+`macaddress` STRING, `systemtime` STRING, `endtime` STRING, `runtime` STRING, `starttime` STRING, 
+`cpu` DOUBLE, `cpu_temp` STRING, `diskusage` STRING, `memory` DOUBLE, `id` STRING, 
+`temperature` STRING, `adjtemp` STRING, `adjtempf` STRING, `temperaturef` STRING, 
+`pressure` DOUBLE, `humidity` DOUBLE, `lux` DOUBLE, `proximity` BIGINT, 
+`oxidising` DOUBLE, `reducing` DOUBLE, `nh3` DOUBLE, `gasko` STRING)
+STORED AS ORC
+LOCATION '/tmp/sensors';
+
