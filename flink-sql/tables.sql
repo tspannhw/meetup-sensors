e21f2ac4c06e78cc32ac3531d833e2cd95d2327d
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-05-04 15:06:23 -0400

    Update tables.sql

--- flink-sql/tables.sql
+++ flink-sql/tables.sql
@@ -50,3 +50,49 @@ CREATE TABLE energy (
 	'connector.properties.group.id' = 'flink-sql-energy-consumer',
 	'format.type' = 'json'
 );
+
+
+
+CREATE TABLE scada (
+	uuid STRING, 
+	systemtime STRING,  
+	amplitude100 DOUBLE, 
+        amplitude500 DOUBLE, 
+	amplitude1000 DOUBLE, 
+	lownoise DOUBLE, 
+	midnoise DOUBLE,
+        highnoise DOUBLE, 
+	amps DOUBLE, 
+	ipaddress STRING, 
+	host STRING, 
+	host_name STRING,
+        macaddress STRING, 
+	endtime STRING, 
+	runtime STRING, 
+	starttime STRING, 
+        cpu DOUBLE, 
+	cpu_temp STRING, 
+	diskusage STRING, 
+	memory DOUBLE, 
+	id STRING, 
+	temperature STRING, 
+	adjtemp STRING, 
+	adjtempf STRING, 
+	temperaturef STRING, 
+	pressure DOUBLE, 
+	humidity DOUBLE, 
+	lux DOUBLE, 
+	proximity INT, 
+	oxidising DOUBLE, 
+	reducing DOUBLE, 
+	nh3 DOUBLE, 
+	gasko STRING
+) WITH (
+	'connector.type'    	 = 'kafka',
+	'connector.version' 	 = 'universal',
+	'connector.topic'   	 = 'scada',
+	'connector.startup-mode' = 'earliest-offset',
+	'connector.properties.bootstrap.servers' = 'tspann-princeton0-cluster-0.general.fuse.l42.cloudera.com:9092',
+	'connector.properties.group.id' = 'flink-sql-scada-consumer',
+	'format.type' = 'json'
+);
