2664d8081e901c462e1449478e7747ecfde43bb4
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-06-18 10:24:25 -0400

    Update kudutables.sql

--- flink-sql/kudutables.sql
+++ flink-sql/kudutables.sql
@@ -86,3 +86,27 @@ uuid STRING,
  PRIMARY KEY (uuid) ) 
 PARTITION BY HASH PARTITIONS 4 
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+CREATE TABLE global_sensor_events 
+(uuid STRING, 
+ systemtime STRING, 
+ temperaturef STRING, 
+ pressure DOUBLE, 
+ humidity DOUBLE, 
+ lux DOUBLE, 
+ proximity INT, 
+ oxidising DOUBLE, 
+ reducing DOUBLE, 
+ nh3 DOUBLE, 
+ gasko STRING, 
+ `current` INT, 
+ voltage INT,
+ `power` INT, 
+ `total` INT, 
+ fanstatus STRING,
+  PRIMARY KEY (uuid) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+ 
+
