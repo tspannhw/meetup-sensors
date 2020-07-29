fb2cf08b0fa5e6b67b35a1f6ebabaa580569e0a7
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-29 12:07:19 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,90 @@
+
+# No Kerberos 
+
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/admin
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/root
+HADOOP_USER_NAME=hdfs hdfs dfs -chown root:root /user/root
+HADOOP_USER_NAME=hdfs hdfs dfs -chown admin:admin /user/admin
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /user
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/kafka
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /user/kafka
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/sensorevents/
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/sensorevents/
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/sensorevents/
+
+impala-shell -i ec2-52-54-225-47.compute-1.amazonaws.com -d default -f kudu.sql 
+
+curl -k -u admin:supersecret1 --location --request POST 'http://ec2-52-54-225-47.compute-1.amazonaws.com:31000/api/atlas/v2/types/typedefs' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "enumDefs": [],
+    "structDefs": [],
+    "classificationDefs": [],
+    "entityDefs": [
+        {
+            "name": "flink_application",
+            "superTypes": [
+                "Process"
+            ],
+            "serviceType": "flink",
+            "typeVersion": "1.0",
+            "attributeDefs": [
+                {
+                    "name": "id",
+                    "typeName": "string",
+                    "cardinality": "SINGLE",
+                    "isIndexable": true,
+                    "isOptional": false,
+                    "isUnique": true
+                },
+                {
+                    "name": "startTime",
+                    "typeName": "date",
+                    "cardinality": "SINGLE",
+                    "isIndexable": false,
+                    "isOptional": true,
+                    "isUnique": false
+                },
+                {
+                    "name": "endTime",
+                    "typeName": "date",
+                    "cardinality": "SINGLE",
+                    "isIndexable": false,
+                    "isOptional": true,
+                    "isUnique": false
+                },
+                {
+                    "name": "conf",
+                    "typeName": "map<string,string>",
+                    "cardinality": "SINGLE",
+                    "isIndexable": false,
+                    "isOptional": true,
+                    "isUnique": false
+                },
+                {
+                    "name": "inputs",
+                    "typeName": "array<string>",
+                    "cardinality": "LIST",
+                    "isIndexable": false,
+                    "isOptional": false,
+                    "isUnique": false
+                },
+                {
+                    "name": "outputs",
+                    "typeName": "array<string>",
+                    "cardinality": "LIST",
+                    "isIndexable": false,
+                    "isOptional": false,
+                    "isUnique": false
+                }
+            ]
+        }
+    ],
+    "relationshipDefs": []
+}'
